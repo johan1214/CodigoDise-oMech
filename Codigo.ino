98f645d32b5f2ef93edbf3455862e757d3d9a5e0
johan1214 <74885410+johan1214@users.noreply.github.com>
2023-01-25 23:25:53 -0500

    Update Codigo.ino

--- Codigo.ino
+++ Codigo.ino
@@ -1,78 +1,118 @@
-const int Trigger1 = 13;   //Pin digital 2 para el Trigger del sensor
-const int Echo1 = 12;   //Pin digital 3 para el Echo del sensor
+const int Trigger1 = 13;
+const int Echo1 = 12;
+const int Trigger2 = 7;
+const int Echo2 = 6;
+const int Trigger3 = 3;
+const int Echo3 = 2;
+const int Motores = 10;
+const int Camara = 14;
+const int Buzzer = 4;
+int EstadoBoton;
+int EstadoBoton1;
+int EstadoBoton2;
 
+bool estado = false;
+bool estado1 = false;
+bool estado2 = false;
+int but = 8;
+int but1 = 9;
+int but2 = 5;
 
-const int Trigger2 = 7;   //Pin digital 8 para el Trigger del sensor
-const int Echo2 = 6;   //Pin digital 9 para el Echo del sensor
-
-
-const int Trigger3 = 3;   //Pin digital 11 para el Trigger del sensor
-const int Echo3 = 2;   //Pin digital 12 para el Echo del sensor
-
-
-const int Motores= 10; //Pin digital 10 para los motores
-
-const int Camara= 14;
-
-const int Buzzer= 4;
 void setup() {
-  Serial.begin(9600);//iniciailzamos la comunicación
-  pinMode(Trigger1, OUTPUT); //pin como salida
-  pinMode(Echo1, INPUT);  //pin como entrada
-  digitalWrite(Trigger1, LOW);//Inicializamos el pin con 0
-  pinMode(Trigger2, OUTPUT); //pin como salida
-  pinMode(Echo2, INPUT);  //pin como entrada
-  digitalWrite(Trigger2, LOW);//Inicializamos el pin con 0
-  pinMode(Trigger3, OUTPUT); //pin como salida
-  pinMode(Echo3, INPUT);  //pin como entrada
+  Serial.begin(9600);
+  pinMode(Trigger1, OUTPUT);
+  pinMode(Echo1, INPUT);
+  digitalWrite(Trigger1, LOW);
+  pinMode(Trigger2, OUTPUT);
+  pinMode(Echo2, INPUT);
+  digitalWrite(Trigger2, LOW);
+  pinMode(Trigger3, OUTPUT);
+  pinMode(Echo3, INPUT);
   pinMode(Camara, INPUT);
   pinMode(Motores, OUTPUT);
   pinMode(Buzzer, OUTPUT);
-  digitalWrite(Trigger3, LOW);//Inicializamos el pin con 0
+  pinMode(but, INPUT);
+  pinMode(but1, INPUT);
+  pinMode(but2, INPUT);
+
+
+
+  digitalWrite(Trigger3, LOW);
 }
 
 
-void loop()
-{
+void loop() {
   bool b;
-  long d1=obtenerDistancia(Trigger1,Echo1);
-  long d2=obtenerDistancia(Trigger2,Echo2);
-  long d3=obtenerDistancia(Trigger3,Echo3);
-  b=obtenerCamara(Camara);
-  if(d1<= 30 || d2 <=30 || d3<=30){
-    digitalWrite(Motores, LOW);
-    digitalWrite(Buzzer,HIGH);
+  long d1 = obtenerDistancia(Trigger1, Echo1);
+  long d2 = obtenerDistancia(Trigger2, Echo2);
+  long d3 = obtenerDistancia(Trigger3, Echo3);
+  b = obtenerCamara(Camara);
+
+
+  EstadoBoton = digitalRead(but);
+  EstadoBoton1 = digitalRead(but1);
+  EstadoBoton2 = digitalRead(but2);
+
+  if ((EstadoBoton == HIGH)) {
+    estado = !estado;
   }
-  else if(b){
-    digitalWrite(Motores, LOW);
-    digitalWrite(Buzzer,HIGH);
+  if ((EstadoBoton1 == HIGH)) {
+    estado1 = !estado1;
   }
-  else{
+  if ((EstadoBoton2 == HIGH)) {
+    estado2 = !estado1;
+  }
+
+  if (estado == true) {
     digitalWrite(Motores, HIGH);
-    digitalWrite(Buzzer,LOW);
+    Serial.print("MOTOR ON");
+    if (d1 <= 30 || d2 <= 30 || d3 <= 30) {
+      Serial.print("COITO");
+      digitalWrite(Motores, LOW);
+      digitalWrite(Buzzer, HIGH);
+    } else if (b) {
+
+      digitalWrite(Motores, LOW);
+      digitalWrite(Buzzer, HIGH);
+    } else {
+      digitalWrite(Motores, HIGH);
+      digitalWrite(Buzzer, LOW);
+    }
+    delay(10);
+
+
+
+  } else {
+    digitalWrite(Motores, LOW);
   }
-  delay(10); 
+  if (estado1 == true) {
+    digitalWrite(Buzzer, HIGH);
+    Serial.print("BUT2 ON");
+
+
+
+  } else {
+    digitalWrite(Buzzer, LOW);
+  }
 }
 
 
-long obtenerDistancia(int Trigger, int Echo){
-    long t; //timepo que demora en llegar el eco
-    long d; //distancia en centimetros
-    
-    
-    digitalWrite(Trigger, HIGH);
-    delayMicroseconds(10);          //Enviamos un pulso de 10us
-    digitalWrite(Trigger, LOW);
-    t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
-    d = t/59;             //escalamos el tiempo a una distancia en cm
-    return d;
-          
+long obtenerDistancia(int Trigger, int Echo) {
+  long t;
+  long d;
+
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trigger, LOW);
+  t = pulseIn(Echo, HIGH);
+  d = t / 59;
+  return d;
 }
 
-bool obtenerCamara(int Camara){
+bool obtenerCamara(int Camara) {
   //Contiene el código de la IA que interpreta la imagen que recibe a partir de la salida serial de la cámara (Pin Camara)
   //el algoritmo interpreta y envía un True en caso de que algun objeto se muestre frente a la cámara ocupando un gran tamaño
   //significando que hay un objeto cerca, envía un False cuando no se esté mostrando nada
-  bool b=false;
+  bool b = false;
   return b;
 }
